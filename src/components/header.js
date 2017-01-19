9ca6ce560bf8c847e599c79147ee8ef6d7252aa7
Ryan Newton <newton1988@gmail.com>
2017-01-18 23:44:24 -0500

    Adds styling to Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-	return <Text>Albums!</Text>;
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
+	);
+};
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		elevation: 3,
+		position: 'relative'
+	},
+	textStyle: {
+		fontSize: 20
+	}
 };
 
 export default Header;
