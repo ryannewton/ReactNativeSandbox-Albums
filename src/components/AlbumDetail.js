6fcc83f1e13cf62a181ba95ee3076f3cf1997368
Ryan Newton <newton1988@gmail.com>
2017-01-20 16:09:13 -0500

    Adds styling to AlbumDetail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,22 +5,49 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 	const { title, artist, thumbnail_image } = album;
+	const {
+		headerContentStyle,
+		headerTextStyle,
+		thumbnailStyle,
+		thumbnailContainerStyle
+	} = styles;
 
 	return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style={thumbnailContainerStyle}>
 					<Image
+						style={thumbnailStyle}
 						source={{ uri: thumbnail_image }}
 					/>
 				</View>
-				<View>
-					<Text>{title}</Text>
+				<View style={headerContentStyle}>
+					<Text style={headerTextStyle}>{title}</Text>
 					<Text>{artist}</Text>
 				</View>
 			</CardSection>
 		</Card>
 	);
+};
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justfiyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	}
 };
 
 export default AlbumDetail;
