93731e04a7ab7619d3f11b4fae45dd8f2303e008
Ryan Newton <newton1988@gmail.com>
2017-01-20 13:59:23 -0500

    Places AlbumDetail within a Card component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = (props) => {
 	return (
-
+		<Card>
+			<Text>{props.album.title}</Text>
+		</Card>
 	);
 };
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ import { View } from 'react-native';
 const Card = (props) => {
 	return(
 		<View style={styles.containerStyle}>
+			{props.children}
 		</View>
 	);
 };
