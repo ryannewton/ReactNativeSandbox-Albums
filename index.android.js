a828f9be853633324c5181be93b0be7102f471c1
Ryan Newton <newton1988@gmail.com>
2017-01-20 16:34:59 -0500

    Adds scrolling

--- index.android.js
+++ index.android.js
@@ -4,7 +4,8 @@ import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-	<View>
+	<View style={{ flex: 1 }}>
+	{/*Gotcha Note: `style={{ flex:1 }}` is necessary to allow for proper scrolling behavior*/}
 		<Header headerText={'Albums'} />
 		<AlbumList />
 	</View>
