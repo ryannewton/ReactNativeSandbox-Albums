be322bd673d6ab667c73635312c048ae1a013cd6
Ryan Newton <newton1988@gmail.com>
2017-01-20 03:28:17 -0500

    Fetches album info and renders titles on AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+	state = { albums: [] };
+
+	componentWillMount() {
+		fetch('http://rallycoding.herokuapp.com/api/music_albums')
+			.then((response) => response.json())
+			.then((responseData) => this.setState({ albums: responseData }))
+			.catch((err) => console.log(err));
+	}
+
+	renderAlbums() {
+		return this.state.albums.map((album) =>
+			<Text key={album.title}>{album.title}</Text>
+		);
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>Album List</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}
